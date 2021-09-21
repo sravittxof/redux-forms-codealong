d097e021f6c331cd60dc1fe501866ff3755fe06b
Travis Fox <travis.fox90@gmail.com>
2021-09-20 21:56:54 -0400

    commit tree 8b0d2e716cf113b0fc9ffc929ed1a8efb7e43d94

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import manageTodo from './reducers/manageTodo';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-const store = createStore(manageTodo);
+const store = createStore(manageTodo, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
